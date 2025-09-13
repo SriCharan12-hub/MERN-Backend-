4aaedf36f3181da55703bd278828ea930bb92ec9
sricharan12-hub <sricharanpalem07@gmail.com>
2025-09-13 10:18:56 +0530

    message2

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ import route from "./Route/Userroute.js"
 dotenv.config()
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    methods: ["GET", "POST", "PUT", "DELETE"]
+}))
 
 app.use(route)
 
